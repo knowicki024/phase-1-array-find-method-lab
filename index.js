7dd3183585aa959f42f1df13de95f5e7dc9e14ba
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-11-19 17:59:56 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array) {
+    const winningYear = array.find(object => object.result === "W");
+    if (winningYear) {
+        return winningYear.year;
+    } else {
+        return undefined;
+    }
+}
